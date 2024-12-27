9d980f8a157a3d2311a3489fd44f89622b607c76
Igors Koptevs <igors.koptevs@gmail.com>
2024-12-27 16:42:20 +0200

    Added default expert value to Users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('expert_number');
+            $table->string('expert_number')->default('00');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
